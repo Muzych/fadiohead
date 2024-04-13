87d8b8c5d0a9ae2c38499e4834d0fd3385fd7d1f
Li0329 <haxk1024@gmail.com>
2024-04-13 22:04:47 +0800

    add background

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -24,8 +24,19 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="text-[#4a4a4a] text-[1em] font-normal leading-normal">
+      <div id="home" className="h-[99vh] overflow-hidden bg-[#000] w-screen">
+        <div className="bg">
+          <div id="top" className="w-screen h-[37vh] bg-white"></div>
+          <div id="mid" className="w-screen h-[25.8vh] bg-[#f60]">
+            <div id="up" className="[border-top:13vh_solid_#fff] w-[0] h-[0] [border-left:50vw_solid_transparent] [border-right:50vw_solid_transparent]"></div>
+            <div id="down" className="[border-bottom:13vh_solid_#000] w-[0] h-[0] [border-left:50vw_solid_transparent] [border-right:50vw_solid_transparent]"></div>
+          </div>
+          <div id="bottom" className="w-screen h-[37vh] bg-[#000]"></div>
+        </div>
+        <div className="bg">
+        </div>
+      </div>
+    </main>
   );
 }
